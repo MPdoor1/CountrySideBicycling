a63a97482506d1dfa6332043ece176e22e6ba41f
MPdoor1 <107142603+MPdoor1@users.noreply.github.com>
2022-12-29 16:53:56 -0500

    Made the home page

--- CountrySideBicylcling/wwwroot/js/site.js
+++ CountrySideBicylcling/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+window.addEventListener('scroll', function () {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        navbar.style.backgroundColor = '#d3d3d3';
+        navbar.style.height = '50px';
+    } else {
+        navbar.style.backgroundColor = 'transparent';
+        navbar.style.height = '100px';
+    }
+});
