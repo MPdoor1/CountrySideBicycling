b5b052668843992beac9ced0d11be6cc66022053
MPdoor1 <107142603+MPdoor1@users.noreply.github.com>
2023-01-02 19:46:40 -0500

    Added scooter files

---
+++ CountrySideBicylcling/Pages/Scooters/ElectricScooter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CountrySideBicylcling.Pages.Scooters
+{
+    public class ElectricScooterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
