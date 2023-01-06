604cb0ef1fdfb16386488fb2ed908199ec0dedb1
MPdoor1 <107142603+MPdoor1@users.noreply.github.com>
2023-01-05 21:22:11 -0500

    Finished index page

--- CountrySideBicylcling/wwwroot/js/site.js
+++ CountrySideBicylcling/wwwroot/js/site.js
@@ -7,9 +7,11 @@ window.addEventListener('scroll', function () {
     var navbar = document.getElementById('navbar');
     if (window.scrollY > 0) {
         navbar.style.backgroundColor = '#d3d3d3';
+        navbar.style.opacity = '100%';
         navbar.style.height = '50px';
     } else {
-        navbar.style.backgroundColor = 'transparent';
-        navbar.style.height = '100px';
+        /*navbar.style.backgroundColor = 'transparent';*/
+        navbar.style.height = '80px';
+        navbar.style.opacity = '90%';
     }
 });
